c24c3d21fe325d7856fa82622f47eab8adf21a13
l-yang-05 <40499685+l-yang-05@users.noreply.github.com>
2019-10-08 18:18:39 -0400

    Added contact api from netlify for form

--- src/Components/Contact/index.jsx
+++ src/Components/Contact/index.jsx
@@ -1,15 +1,44 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import useForm from 'react-hook-form';
 
+const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+}
+
 const Contact = () => {
+    const [state, setState] = useState({ fullName: "", email: "", message: "" })
 
     const { register, handleSubmit, errors } = useForm();
 
-    const onSubmit = (data, e) => {
+    const onSubmit = (state, e) => {
+        contactAPI(state)
         alert("Thank you for reaching out to me! :)")
-        console.log(data);
+        e.target.value()
     }
     console.log(errors);
+
+
+    const contactAPI = async () => {
+        try {
+            const res = await fetch("/", {
+                method: "POST",
+                headers: { "Content-Type": "application/x-www-form-urlencoded" },
+                body: encode({ "form-name": "contact", state })
+            })
+            const content = await res.json()
+            console.log(content);
+        }
+        catch (error) {
+            throw error;
+        }
+    }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setState(prevState => ({ ...prevState, [name]: value }));
+    }
 
     useEffect(() => {
         document.title = "Lucy Yang | Contact"
@@ -20,18 +49,18 @@ const Contact = () => {
             <h1>Contact Me!</h1>
             <div className="form-wrapper">
                 <fieldset>
-                    <form name="contact-me" onSubmit={handleSubmit(onSubmit)} method="POST" data-netlify="true">
+                    <form form-name="contact-me" onSubmit={handleSubmit(onSubmit)} method="POST" data-netlify="true">
 
                         <label htmlFor="fname">Full Name</label>
-                        <input type="text" placeholder="Full Name" name="full_name" id="fname" ref={register({ required: true, min: 5, pattern: /^[a-zA-z']([^0-9]*)$/ })} />
+                        <input type="text" placeholder="Full Name" onChange={handleChange} name="full_name" id="fname" value={state.fullName} ref={register({ required: true, min: 5, pattern: /^[a-zA-z']([^0-9]*)$/ })} />
                         <p className="error-msg">{errors.full_name && "Please enter in your full name! Make sure you're only submitting in letter characters in this field!!!"}</p>
 
                         <label htmlFor="mail">Email Address</label>
-                        <input type="text" placeholder="Email address" name="email" id="mail" ref={register({ required: true, pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ })} />
+                        <input type="text" placeholder="Email address" onChange={handleChange} name="email" id="mail" value={state.email} ref={register({ required: true, pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ })} />
                         <p className="error-msg">{errors.email && 'Please enter in a vaild email address!'}</p>
 
                         <label htmlFor="msg">Message</label>
-                        <textarea name="message" placeholder="Write your message in here!" id="msg" ref={register({ required: true, min: 2, max: 1000, maxLength: 200 })}></textarea>
+                        <textarea name="message" placeholder="Write your message in here!" id="msg" onChange={handleChange} value={state.message} ref={register({ required: true, min: 2, max: 1000, maxLength: 200 })}></textarea>
                         <p className="error-msg">{errors.message && 'Please enter in a message!'}</p>
 
                         <button type="submit" value="Submit" name="submit" id="sub">Submit</button>
