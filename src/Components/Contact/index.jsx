fbc7b02d3c08530782e6c816f07785f1782a5c6d
l-yang-05 <40499685+l-yang-05@users.noreply.github.com>
2019-10-08 18:25:29 -0400

    Took out values in input fields

--- src/Components/Contact/index.jsx
+++ src/Components/Contact/index.jsx
@@ -13,14 +13,14 @@ const Contact = () => {
     const { register, handleSubmit, errors } = useForm();
 
     const onSubmit = (state, e) => {
-        contactAPI(state)
+        contactAPI(state).then(e.preventDefault())
         alert("Thank you for reaching out to me! :)")
-        e.target.value()
+        // e.preventDefault()
     }
     console.log(errors);
 
 
-    const contactAPI = async () => {
+    const contactAPI = async (state) => {
         try {
             const res = await fetch("/", {
                 method: "POST",
@@ -52,15 +52,15 @@ const Contact = () => {
                     <form form-name="contact-me" onSubmit={handleSubmit(onSubmit)} method="POST" data-netlify="true">
 
                         <label htmlFor="fname">Full Name</label>
-                        <input type="text" placeholder="Full Name" onChange={handleChange} name="full_name" id="fname" value={state.fullName} ref={register({ required: true, min: 5, pattern: /^[a-zA-z']([^0-9]*)$/ })} />
+                        <input type="text" placeholder="Full Name" onChange={handleChange} name="full_name" id="fname" ref={register({ required: true, min: 5, pattern: /^[a-zA-z']([^0-9]*)$/ })} />
                         <p className="error-msg">{errors.full_name && "Please enter in your full name! Make sure you're only submitting in letter characters in this field!!!"}</p>
 
                         <label htmlFor="mail">Email Address</label>
-                        <input type="text" placeholder="Email address" onChange={handleChange} name="email" id="mail" value={state.email} ref={register({ required: true, pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ })} />
+                        <input type="text" placeholder="Email address" onChange={handleChange} name="email" id="mail" ref={register({ required: true, pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ })} />
                         <p className="error-msg">{errors.email && 'Please enter in a vaild email address!'}</p>
 
                         <label htmlFor="msg">Message</label>
-                        <textarea name="message" placeholder="Write your message in here!" id="msg" onChange={handleChange} value={state.message} ref={register({ required: true, min: 2, max: 1000, maxLength: 200 })}></textarea>
+                        <textarea name="message" placeholder="Write your message in here!" id="msg" onChange={handleChange} ref={register({ required: true, min: 2, max: 1000, maxLength: 200 })}></textarea>
                         <p className="error-msg">{errors.message && 'Please enter in a message!'}</p>
 
                         <button type="submit" value="Submit" name="submit" id="sub">Submit</button>
