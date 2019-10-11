e3ad94a52f75167f3e5f5e0b968fd32f97802c17
l-yang-05 <40499685+l-yang-05@users.noreply.github.com>
2019-10-11 00:15:03 -0400

    Finished adding thumbnails to site and fixed media queries

--- src/Components/Portfolio/index.jsx
+++ src/Components/Portfolio/index.jsx
@@ -1,4 +1,9 @@
 import React, { useEffect } from 'react';
+import ecom from '../../images/ecom.png';
+import gateway from '../../images/gateway-html.png';
+import number from '../../images/number-guess.png';
+import comment from '../../images/comment-app.png';
+import calculator from '../../images/calculator.png'
 
 const Portfolio = () => {
     useEffect(() => {
@@ -11,22 +16,28 @@ const Portfolio = () => {
                 <h2 className="work-intro">Coding Projects</h2>
                 <div className="work-container">
                     <a href="https://github.com/l-yang-05/Kiwis-Paradise-v2" target="_blank" aria-label="eCommerce Full stack project's repo" rel="noopener noreferrer"><div className="work-item">
-                        <p>Full stack eCommerce webstie</p>
+                        <img className="screenshot" src={ecom} alt="eCommerece site"></img>
+                        <p>Full stack eCommerce</p>
                     </div></a>
-                    <a href="https://github.com/l-yang-05/Kiwis-Paradise-v2" target="_blank" aria-label="eCommerce Full stack project's repo" rel="noopener noreferrer"><div className="work-item">
-                        <p>Full stack eCommerce webstie</p>
+                    <a href="https://github.com/l-yang-05/gateway-html" target="_blank" aria-label="Gateway repo" rel="noopener noreferrer"><div className="work-item">
+                        <img className="screenshot" src={gateway} alt="Gateway site"></img>
+                        <p>Kyoto Japan Gateway</p>
                     </div></a>
-                    <a href="https://github.com/l-yang-05/Kiwis-Paradise-v2" target="_blank" aria-label="eCommerce Full stack project's repo" rel="noopener noreferrer"><div className="work-item">
-                        <p>Full stack eCommerce webstie</p>
+                    <a href="https://github.com/l-yang-05/Number-Guessing-Game" target="_blank" aria-label="Number guessing game repo" rel="noopener noreferrer"><div className="work-item">
+                        <img className="screenshot" src={number} alt="Number Guessing Game"></img>
+                        <p>Number Guessing Game</p>
                     </div></a>
-                    <a href="https://github.com/l-yang-05/Kiwis-Paradise-v2" target="_blank" aria-label="eCommerce Full stack project's repo" rel="noopener noreferrer"><div className="work-item">
-                        <p>Full stack eCommerce webstie</p>
+                    <a href="https://github.com/l-yang-05/API-Node-Challenges" target="_blank" aria-label="API node challenges" rel="noopener noreferrer"><div className="work-item">
+                        <img className="screenshot" src="https://www.digital-loggers.com/images/rest.png" alt="API node challenges"></img>
+                        <p>API Node Challenge</p>
                     </div></a>
-                    <a href="https://github.com/l-yang-05/Kiwis-Paradise-v2" target="_blank" aria-label="eCommerce Full stack project's repo" rel="noopener noreferrer"><div className="work-item">
-                        <p>Full stack eCommerce webstie</p>
+                    <a href="https://github.com/l-yang-05/Comment-app" target="_blank" aria-label="Comment app repo" rel="noopener noreferrer"><div className="work-item">
+                        <img className="screenshot" src={comment} alt="Number Guessing Game"></img>
+                        <p>React Comment App</p>
                     </div></a>
-                    <a href="https://github.com/l-yang-05/Kiwis-Paradise-v2" target="_blank" aria-label="eCommerce Full stack project's repo" rel="noopener noreferrer"><div className="work-item">
-                        <p>Full stack eCommerce webstie</p>
+                    <a href="https://github.com/l-yang-05/calculator-js" target="_blank" aria-label="JS Calculator repo" rel="noopener noreferrer"><div className="work-item">
+                        <img className="screenshot" src={calculator} alt="JavaScript Calculator"></img>
+                        <p>JS Calculator</p>
                     </div></a>
                 </div>
             </section>
