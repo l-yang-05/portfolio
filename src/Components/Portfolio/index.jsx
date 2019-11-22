1ead2acf2f5769adac3451feff9a489949ee012e
l-yang-05 <40499685+l-yang-05@users.noreply.github.com>
2019-11-22 11:51:34 -0500

    Updated projects

--- src/Components/Portfolio/index.jsx
+++ src/Components/Portfolio/index.jsx
@@ -3,7 +3,8 @@ import ecom from '../../images/ecom.png';
 import gateway from '../../images/gateway-html.png';
 import number from '../../images/number-guess.png';
 import comment from '../../images/comment-app.png';
-import calculator from '../../images/calculator.png'
+import hiredUp from '../../images/hiredUp.png';
+import admin from '../../images/adminsite.png'
 
 const Portfolio = () => {
     useEffect(() => {
@@ -15,9 +16,13 @@ const Portfolio = () => {
             <section>
                 <h2 className="work-intro">Coding Projects</h2>
                 <div className="work-container">
-                    <a href="https://github.com/l-yang-05/Kiwis-Paradise-v2" target="_blank" aria-label="eCommerce Full stack project's repo" rel="noopener noreferrer"><div className="work-item">
+                    <a href="https://github.com/l-yang-05/KiwisParadise-Mongo" target="_blank" aria-label="eCommerce Full stack project's repo" rel="noopener noreferrer"><div className="work-item">
                         <img className="screenshot" src={ecom} alt="eCommerece site"></img>
                         <p>Full stack eCommerce</p>
+                    </div></a>
+                    <a href="https://github.com/l-yang-05/KiwisParadise-Admin" target="_blank" aria-label="JS Calculator repo" rel="noopener noreferrer"><div className="work-item">
+                        <img className="screenshot" src={admin} alt="Admin site"></img>
+                        <p>C# eCommerce Admin site</p>
                     </div></a>
                     <a href="https://github.com/l-yang-05/gateway-html" target="_blank" aria-label="Gateway repo" rel="noopener noreferrer"><div className="work-item">
                         <img className="screenshot" src={gateway} alt="Gateway site"></img>
@@ -25,19 +30,15 @@ const Portfolio = () => {
                     </div></a>
                     <a href="https://github.com/l-yang-05/Number-Guessing-Game" target="_blank" aria-label="Number guessing game repo" rel="noopener noreferrer"><div className="work-item">
                         <img className="screenshot" src={number} alt="Number Guessing Game"></img>
-                        <p>Number Guessing Game</p>
+                        <p> React Number Guessing Game</p>
                     </div></a>
-                    <a href="https://github.com/l-yang-05/API-Node-Challenges" target="_blank" aria-label="API node challenges" rel="noopener noreferrer"><div className="work-item">
-                        <img className="screenshot" src="https://www.digital-loggers.com/images/rest.png" alt="API node challenges"></img>
-                        <p>API Node Challenge</p>
+                    <a href="https://github.com/l-yang-05/HiredUp" target="_blank" aria-label="API node challenges" rel="noopener noreferrer"><div className="work-item">
+                        <img className="screenshot" src={hiredUp} alt="API node challenges"></img>
+                        <p>Hired Up App</p>
                     </div></a>
                     <a href="https://github.com/l-yang-05/Comment-app" target="_blank" aria-label="Comment app repo" rel="noopener noreferrer"><div className="work-item">
-                        <img className="screenshot" src={comment} alt="Number Guessing Game"></img>
+                        <img className="screenshot" src={comment} alt="Comment App"></img>
                         <p>React Comment App</p>
-                    </div></a>
-                    <a href="https://github.com/l-yang-05/calculator-js" target="_blank" aria-label="JS Calculator repo" rel="noopener noreferrer"><div className="work-item">
-                        <img className="screenshot" src={calculator} alt="JavaScript Calculator"></img>
-                        <p>JS Calculator</p>
                     </div></a>
                 </div>
             </section>
