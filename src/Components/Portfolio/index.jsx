1a8227b5f9fa4e9a1f13785487cbcfccaa375861
l-yang-05 <40499685+l-yang-05@users.noreply.github.com>
2019-11-23 16:12:57 -0500

    Updated projects

--- src/Components/Portfolio/index.jsx
+++ src/Components/Portfolio/index.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
 import ecom from '../../images/ecom.png';
-import gateway from '../../images/gateway-html.png';
 import number from '../../images/number-guess.png';
-import comment from '../../images/comment-app.png';
 import hiredUp from '../../images/hiredUp.png';
-import admin from '../../images/adminsite.png'
+import admin from '../../images/adminsite.png';
+import slade from '../../images/slade.png';
+import starCatcher from '../../images/star-catcher.png';
 
 const Portfolio = () => {
     useEffect(() => {
@@ -18,15 +18,15 @@ const Portfolio = () => {
                 <div className="work-container">
                     <a href="https://github.com/l-yang-05/KiwisParadise-Mongo" target="_blank" aria-label="eCommerce Full stack project's repo" rel="noopener noreferrer"><div className="work-item">
                         <img className="screenshot" src={ecom} alt="eCommerece site"></img>
-                        <p>Full stack eCommerce</p>
+                        <p>Full stack MERN eCommerce</p>
                     </div></a>
                     <a href="https://github.com/l-yang-05/KiwisParadise-Admin" target="_blank" aria-label="JS Calculator repo" rel="noopener noreferrer"><div className="work-item">
                         <img className="screenshot" src={admin} alt="Admin site"></img>
                         <p>C# eCommerce Admin site</p>
                     </div></a>
-                    <a href="https://github.com/l-yang-05/gateway-html" target="_blank" aria-label="Gateway repo" rel="noopener noreferrer"><div className="work-item">
-                        <img className="screenshot" src={gateway} alt="Gateway site"></img>
-                        <p>Kyoto Japan Gateway</p>
+                    <a href="https://github.com/l-yang-05/Star-Catcher" target="_blank" aria-label="star-catcher" rel="noopener noreferrer"><div className="work-item">
+                        <img className="screenshot" src={starCatcher} alt="star-catcher"></img>
+                        <p>MERN Auction eCommerce</p>
                     </div></a>
                     <a href="https://github.com/l-yang-05/Number-Guessing-Game" target="_blank" aria-label="Number guessing game repo" rel="noopener noreferrer"><div className="work-item">
                         <img className="screenshot" src={number} alt="Number Guessing Game"></img>
@@ -36,9 +36,9 @@ const Portfolio = () => {
                         <img className="screenshot" src={hiredUp} alt="API node challenges"></img>
                         <p>Hired Up App</p>
                     </div></a>
-                    <a href="https://github.com/l-yang-05/Comment-app" target="_blank" aria-label="Comment app repo" rel="noopener noreferrer"><div className="work-item">
-                        <img className="screenshot" src={comment} alt="Comment App"></img>
-                        <p>React Comment App</p>
+                    <a href="https://github.com/l-yang-05/SLADE" target="_blank" aria-label="SLADE" rel="noopener noreferrer"><div className="work-item">
+                        <img className="screenshot slade" src={slade} alt="SLADE"></img>
+                        <p>SLADE MERN App</p>
                     </div></a>
                 </div>
             </section>
